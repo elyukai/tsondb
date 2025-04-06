2405ede2ca3596fe43e67971d0eede64ce0eb4a8
Lukas Obermann <lukas.obermann@icloud.com>
2025-04-06 18:37:18 +0200

    test: add test for lazy class

---
+++ test/utils/lazy.ts
@@ -0,0 +1,20 @@
+import { equal } from "assert"
+import { describe, it } from "node:test"
+import { Lazy } from "../../src/utils/lazy.js"
+
+describe("Lazy", () => {
+  it("should only evaluate the given expression when needed", () => {
+    let count = 0
+    const lazyValue = Lazy.of(() => ++count)
+    equal(count, 0)
+    equal(lazyValue.value, 1)
+    equal(count, 1)
+  })
+
+  it("should only evaluate the given expression once", () => {
+    let count = 0
+    const lazyValue = Lazy.of(() => ++count)
+    equal(lazyValue.value, 1)
+    equal(lazyValue.value, 1)
+  })
+})
