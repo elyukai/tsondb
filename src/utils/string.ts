52989e48cf01b199812bc31e0c3e43f3ab3cb0bf
Lukas Obermann <lukas.obermann@icloud.com>
2025-04-18 12:00:51 +0200

    feat: add string case conversion functions

---
+++ src/utils/string.ts
@@ -0,0 +1,70 @@
+const letterOrDigit = /[a-zA-Z0-9]/
+const uppercase = /[A-Z]/
+const lowerCaseOrDigit = /[a-z0-9]/
+const separator = /[^a-z0-9]/
+
+const lastChar = (str: string) => str[str.length - 1]
+const lastElement = <T>(arr: T[]) => arr[arr.length - 1]
+
+export const splitStringParts = (str: string): string[] =>
+  [...str].reduce((acc: string[], char, i, strArr) => {
+    if (acc.length === 0) {
+      return letterOrDigit.test(char) ? [char] : acc
+    }
+
+    const lastPart = lastElement(acc)!
+
+    if (uppercase.test(char)) {
+      const lastCharOfLastPart = lastChar(lastPart)
+      const nextChar = strArr[i + 1]
+
+      if (
+        lastCharOfLastPart === undefined ||
+        (uppercase.test(lastCharOfLastPart) && (nextChar === undefined || separator.test(nextChar)))
+      ) {
+        return [...acc.slice(0, -1), lastPart + char]
+      } else {
+        return [...acc, char]
+      }
+    }
+
+    if (lowerCaseOrDigit.test(char)) {
+      return [...acc.slice(0, -1), lastPart + char]
+    }
+
+    if (lastPart === "") {
+      return acc
+    } else {
+      return [...acc, ""]
+    }
+  }, [])
+
+export const toPascalCase = (str: string): string =>
+  splitStringParts(str)
+    .map(part =>
+      part === part.toUpperCase()
+        ? part
+        : part.charAt(0).toUpperCase() + part.slice(1).toLowerCase(),
+    )
+    .join("")
+
+export const toCamelCase = (str: string): string =>
+  splitStringParts(str)
+    .map((part, i) =>
+      i === 0
+        ? part.toLowerCase()
+        : part === part.toUpperCase()
+        ? part
+        : part.charAt(0).toUpperCase() + part.slice(1).toLowerCase(),
+    )
+    .join("")
+
+export const toKebabCase = (str: string): string =>
+  splitStringParts(str)
+    .map(part => part.toLowerCase())
+    .join("-")
+
+export const toSnakeCase = (str: string): string =>
+  splitStringParts(str)
+    .map(part => part.toLowerCase())
+    .join("_")
---
+++ test/utils/string.ts
@@ -0,0 +1,158 @@
+import { deepEqual, equal } from "assert/strict"
+import { describe, it } from "node:test"
+import {
+  splitStringParts,
+  toCamelCase,
+  toKebabCase,
+  toPascalCase,
+  toSnakeCase,
+} from "../../src/utils/string.js"
+
+describe("splitStringParts", () => {
+  it("should split strings of various cases into its parts", () => {
+    deepEqual(splitStringParts("PascalCase"), ["Pascal", "Case"])
+    deepEqual(splitStringParts("camelCase"), ["camel", "Case"])
+    deepEqual(splitStringParts("snake_case"), ["snake", "case"])
+    deepEqual(splitStringParts("kebab-case"), ["kebab", "case"])
+    deepEqual(splitStringParts("UPPER_CASE"), ["UPPER", "CASE"])
+    deepEqual(splitStringParts("UPPERCASE"), ["UPPERCASE"])
+    deepEqual(splitStringParts("lowercase"), ["lowercase"])
+    deepEqual(splitStringParts("stringWithURL"), ["string", "With", "URL"])
+  })
+})
+
+describe("toPascalCase", () => {
+  it("should convert PascalCase to PascalCase", () => {
+    equal(toPascalCase("PascalCase"), "PascalCase")
+  })
+
+  it("should convert camelCase to CamelCase", () => {
+    equal(toPascalCase("camelCase"), "CamelCase")
+  })
+
+  it("should convert snake_case to SnakeCase", () => {
+    equal(toPascalCase("snake_case"), "SnakeCase")
+  })
+
+  it("should convert kebab-case to KebabCase", () => {
+    equal(toPascalCase("kebab-case"), "KebabCase")
+  })
+
+  it("should convert UPPER_CASE to UpperCase", () => {
+    equal(toPascalCase("UPPER_CASE"), "UpperCase")
+  })
+
+  it("should convert UPPERCASE to Uppercase", () => {
+    equal(toPascalCase("UPPERCASE"), "Uppercase")
+  })
+
+  it("should convert lowercase to Lowercase", () => {
+    equal(toPascalCase("lowercase"), "Lowercase")
+  })
+
+  it("should convert stringWithURL to StringWithURL", () => {
+    equal(toPascalCase("stringWithURL"), "StringWithURL")
+  })
+})
+
+describe("toCamelCase", () => {
+  it("should convert PascalCase to pascalCase", () => {
+    equal(toCamelCase("PascalCase"), "pascalCase")
+  })
+
+  it("should convert camelCase to camelCase", () => {
+    equal(toCamelCase("camelCase"), "camelCase")
+  })
+
+  it("should convert snake_case to snakeCase", () => {
+    equal(toCamelCase("snake_case"), "snakeCase")
+  })
+
+  it("should convert kebab-case to kebabCase", () => {
+    equal(toCamelCase("kebab-case"), "kebabCase")
+  })
+
+  it("should convert UPPER_CASE to upperCase", () => {
+    equal(toCamelCase("UPPER_CASE"), "upperCase")
+  })
+
+  it("should convert UPPERCASE to uppercase", () => {
+    equal(toCamelCase("UPPERCASE"), "uppercase")
+  })
+
+  it("should convert lowercase to lowercase", () => {
+    equal(toCamelCase("lowercase"), "lowercase")
+  })
+
+  it("should convert stringWithURL to stringWithURL", () => {
+    equal(toCamelCase("stringWithURL"), "stringWithURL")
+  })
+})
+
+describe("toKebabCase", () => {
+  it("should convert PascalCase to pascal-case", () => {
+    equal(toKebabCase("PascalCase"), "pascal-case")
+  })
+
+  it("should convert camelCase to camel-case", () => {
+    equal(toKebabCase("camelCase"), "camel-case")
+  })
+
+  it("should convert snake_case to snake-case", () => {
+    equal(toKebabCase("snake_case"), "snake-case")
+  })
+
+  it("should convert kebab-case to kebab-case", () => {
+    equal(toKebabCase("kebab-case"), "kebab-case")
+  })
+
+  it("should convert UPPER_CASE to upper-case", () => {
+    equal(toKebabCase("UPPER_CASE"), "upper-case")
+  })
+
+  it("should convert UPPERCASE to uppercase", () => {
+    equal(toKebabCase("UPPERCASE"), "uppercase")
+  })
+
+  it("should convert lowercase to lowercase", () => {
+    equal(toKebabCase("lowercase"), "lowercase")
+  })
+
+  it("should convert stringWithURL to string-with-url", () => {
+    equal(toKebabCase("stringWithURL"), "string-with-url")
+  })
+})
+
+describe("toSnakeCase", () => {
+  it("should convert PascalCase to pascal_case", () => {
+    equal(toSnakeCase("PascalCase"), "pascal_case")
+  })
+
+  it("should convert camelCase to camel_case", () => {
+    equal(toSnakeCase("camelCase"), "camel_case")
+  })
+
+  it("should convert snake_case to snake_case", () => {
+    equal(toSnakeCase("snake_case"), "snake_case")
+  })
+
+  it("should convert kebab-case to kebab_case", () => {
+    equal(toSnakeCase("kebab-case"), "kebab_case")
+  })
+
+  it("should convert UPPER_CASE to upper_case", () => {
+    equal(toSnakeCase("UPPER_CASE"), "upper_case")
+  })
+
+  it("should convert UPPERCASE to uppercase", () => {
+    equal(toSnakeCase("UPPERCASE"), "uppercase")
+  })
+
+  it("should convert lowercase to lowercase", () => {
+    equal(toSnakeCase("lowercase"), "lowercase")
+  })
+
+  it("should convert stringWithURL to string_with_url", () => {
+    equal(toSnakeCase("stringWithURL"), "string_with_url")
+  })
+})
