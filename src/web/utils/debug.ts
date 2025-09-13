1fbcd598a32f1b934954193ec1a0e720c397192b
Lukas Obermann <lukas.obermann@icloud.com>
2025-09-13 11:35:38 +0200

    style: remove unnecessary escape character

--- src/web/utils/debug.ts
+++ src/web/utils/debug.ts
@@ -1,6 +1,6 @@
 export const printJson = (data: unknown) =>
   JSON.stringify(data, undefined, 2)
     .replace(/\n *([}\]],?)/g, " $1")
-    .replace(/((?:^|\n *)[{\[])\n +/g, "$1 ")
+    .replace(/((?:^|\n *)[{[])\n +/g, "$1 ")
     .replace(/"(.+?)":/g, '<span style="color: darkorange">$1</span>:')
     .replace(/ "(.*?)"([ ,])/g, ' <span style="color: darkgreen">"$1"</span>$2')
