d1fc25eadd79fb69ee3bb6bbbe104d175928a448
Lukas Obermann <lukas.obermann@icloud.com>
2026-02-05 10:25:32 +0100

    fix: missing exports for type inference

--- src/node/schema/dsl/index.ts
+++ src/node/schema/dsl/index.ts
@@ -2,6 +2,7 @@ import type { TypeParameter } from "./TypeParameter.ts"
 import type { Decl } from "./declarations/Decl.ts"
 import type { Type } from "./types/Type.ts"
 
+export { Param, TypeParameter } from "./TypeParameter.ts"
 export {
   type Decl,
   type DeclP,
@@ -26,6 +27,7 @@ export { Array, ArrayType } from "./types/ArrayType.ts"
 export { Boolean, BooleanType } from "./types/BooleanType.ts"
 export { ChildEntities, ChildEntitiesType } from "./types/ChildEntitiesType.ts"
 export { Date, DateType } from "./types/DateType.ts"
+export { EnumCase, EnumCaseDecl } from "./types/EnumType.ts"
 export { Float, FloatType } from "./types/FloatType.ts"
 export {
   GenIncludeIdentifier,
@@ -35,7 +37,7 @@ export {
 } from "./types/IncludeIdentifierType.ts"
 export { Integer, IntegerType } from "./types/IntegerType.ts"
 export { NestedEntityMap, NestedEntityMapType } from "./types/NestedEntityMapType.ts"
-export { Object, ObjectType, Optional, Required } from "./types/ObjectType.ts"
+export { Object, ObjectType, Optional, Required, type MemberDecl } from "./types/ObjectType.ts"
 export { ReferenceIdentifier, ReferenceIdentifierType } from "./types/ReferenceIdentifierType.ts"
 export { String, StringType } from "./types/StringType.ts"
 export { TranslationObject, TranslationObjectType } from "./types/TranslationObjectType.ts"
