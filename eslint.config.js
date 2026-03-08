cfae0c772fb22f6408caba8df10407ee5d6afdc8
Lukas Obermann <lukas.obermann@icloud.com>
2026-03-08 11:40:12 +0100

    build: exclude rslib config from eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,12 @@
 import js from "@eslint/js"
 import pluginReact from "eslint-plugin-react"
-import reactHooks from 'eslint-plugin-react-hooks'
+import reactHooks from "eslint-plugin-react-hooks"
 import { defineConfig, globalIgnores } from "eslint/config"
 import globals from "globals"
 import tseslint from "typescript-eslint"
 
 export default defineConfig([
-  globalIgnores([
-    "**/dist/",
-  ]),
+  globalIgnores(["**/dist/", "rslib.config.ts"]),
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
     plugins: {
@@ -26,9 +24,7 @@ export default defineConfig([
     },
   },
   {
-    ignores: [
-      "eslint.config.js",
-    ],
+    ignores: ["eslint.config.js"],
     extends: tseslint.configs.strictTypeChecked,
     languageOptions: {
       parserOptions: {
@@ -67,49 +63,49 @@ export default defineConfig([
         "error",
         {
           allowThrowingUnknown: true,
-        }
-      ]
+        },
+      ],
     },
   },
   {
     files: ["test/**/*.{js,mjs,cjs,ts,mts,cts}"],
     rules: {
       "@typescript-eslint/no-floating-promises": "off",
-    }
+    },
   },
   pluginReact.configs.flat.recommended,
   {
     files: ["src/**/*.{js,jsx,ts,tsx}"],
     plugins: {
-      'react-hooks': reactHooks,
+      "react-hooks": reactHooks,
     },
     rules: {
-      'react-hooks/exhaustive-deps': 'warn',
-      'react-hooks/rules-of-hooks': 'error',
+      "react-hooks/exhaustive-deps": "warn",
+      "react-hooks/rules-of-hooks": "error",
     },
   },
   {
-		settings: {
-			react: {
-				// eslint-plugin-preact interprets this as "h.createElement",
-				// however we only care about marking h() as being a used variable.
-				pragma: "h",
-				// We use "react 16.0" to avoid pushing folks to UNSAFE_ methods.
-				version: "16.0",
-			}
-		},
-		rules: {
-			"react/react-in-jsx-scope": "off",
+    settings: {
+      react: {
+        // eslint-plugin-preact interprets this as "h.createElement",
+        // however we only care about marking h() as being a used variable.
+        pragma: "h",
+        // We use "react 16.0" to avoid pushing folks to UNSAFE_ methods.
+        version: "16.0",
+      },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
       "react/no-unknown-property": ["error", { ignore: ["class"] }],
 
-			"react/prop-types": "off",
+      "react/prop-types": "off",
 
-			// Legacy APIs not supported in Preact:
-			"react/no-did-mount-set-state": "error",
-			"react/no-did-update-set-state": "error",
-			"react/no-find-dom-node": "error",
-			"react/no-is-mounted": "error",
-			"react/no-string-refs": "error",
+      // Legacy APIs not supported in Preact:
+      "react/no-did-mount-set-state": "error",
+      "react/no-did-update-set-state": "error",
+      "react/no-find-dom-node": "error",
+      "react/no-is-mounted": "error",
+      "react/no-string-refs": "error",
     },
   },
 ])
